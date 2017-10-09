c71a50fb9b98ab7da169e9249b2ceb502ac9a0f0
Yelitza3125 <deyel_99@hotmail.com>
2017-10-09 18:23:57 -0500

    Subiendo proyecto

---
+++ app.js
@@ -0,0 +1,13 @@
+function isValid(str) {
+	while((typeof(str) === "number")) {
+		var strInvertido = str.reverse();
+	for ( var i = 2; (i % 2 === 0); i +2) {
+		if(strInvertido[i] * 2) {
+		var number = strInvertido.split(strInvertido[i] >= 10);
+		 }		
+		}
+	}
+	
+	function sum();
+	
+}
